5e9d0b81fe4e095c0fa7c04bd09168d6263123d8
e-square-ci <ci@e-sqiaure.io>
2021-06-29 16:37:50 +0000

    fix(utils): rename resolved outputs

--- index.js
+++ index.js
@@ -66572,7 +66572,7 @@ function getWorkspaceProjects() {
 function getProjectOutputs(projects, project, target) {
     var _a;
     const projectTarget = projects[project].targets[target];
-    let outputs = (_a = projectTarget.outputs) !== null && _a !== void 0 ? _a : [];
+    const outputs = (_a = projectTarget.outputs) !== null && _a !== void 0 ? _a : [];
     const replaceExpressions = (path) => {
         var _a, _b, _c;
         if (!path.includes('{') || !path.includes('}'))
@@ -66584,9 +66584,9 @@ function getProjectOutputs(projects, project, target) {
         }
         return (_c = (_b = projectTarget === null || projectTarget === void 0 ? void 0 : projectTarget[scope]) === null || _b === void 0 ? void 0 : _b[prop]) !== null && _c !== void 0 ? _c : '';
     };
-    outputs = outputs.map(replaceExpressions);
-    debug(`🐞 Found ${outputs} as outputs for ${target}`);
-    return outputs;
+    const resolvedOutputs = outputs.map(replaceExpressions);
+    debug(`🐞 Found ${resolvedOutputs} as outputs for ${target}`);
+    return resolvedOutputs;
 }
 function assertNxInstalled() {
     return modules_awaiter(this, void 0, void 0, function* () {
